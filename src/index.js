97d0f7c9802f74b64ad0849ca7e8edd1249e73c4
Johntae Stroud <johntae.stroud@gmail.com>
2018-05-10 14:39:27 -0400

    Implemented hot module replacement

--- src/index.js
+++ src/index.js
@@ -4,5 +4,18 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const rootEL = document.getElementById('root');
+
+let render = () => {
+  ReactDOM.render(<App />, rootEL)
+}
+
+if (module.hot) {
+  module.hot.accept('./App', () => {
+    setTimeout(render)
+  })
+}
+
+render();
+
 registerServiceWorker();
