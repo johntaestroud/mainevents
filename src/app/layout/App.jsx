709130718ae361e730ff9e4ca8d6f0086e9d716a
Johntae Stroud <johntae.stroud@gmail.com>
2018-05-10 17:11:43 -0400

    Implemented first component

--- src/app/layout/App.jsx
+++ src/app/layout/App.jsx
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
-
+import EventDashboard from '../../features/event/EventDashboard/EventDashboard';
 class App extends Component {
   render() {
     return (
       <div>
         <h1>MainEvents</h1>
+          <EventDashboard/>
       </div>
     );
   }
