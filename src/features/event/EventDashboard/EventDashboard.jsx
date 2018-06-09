00fc0946ca19bf005e91092d6220ef0730f55fcf
Johntae Stroud <johntae.stroud@gmail.com>
2018-06-09 14:47:58 -0400

    Event actions to event dashboard connections implemented

--- src/features/event/EventDashboard/EventDashboard.jsx
+++ src/features/event/EventDashboard/EventDashboard.jsx
@@ -4,10 +4,17 @@ import { Grid, Button } from 'semantic-ui-react';
 import cuid from 'cuid';
 import EventList from '../EventList/EventList';
 import EventForm from '../EventForm/EventForm';
+import { createEvent, deleteEvent, updateEvent } from '../eventActions'
 
 const mapState = (state) => ({
   events: state.events
-})
+});
+
+const actions = {
+  createEvent,
+  deleteEvent,
+  updateEvent
+}
 
 class EventDashboard extends Component {
   // constructor(props) {
@@ -33,41 +40,32 @@ class EventDashboard extends Component {
   };
 
   handleUpdateEvent = (updatedEvent) => {
+    this.props.updateEvent(updatedEvent)
     this.setState({
-      events: this.state.events.map(event => {
-        if (event.id === updatedEvent.id) {
-          return Object.assign({}, updatedEvent);
-        } else {
-          return event
-        }
-      }),
       isOpen: false,
       selectedEvent: null
-    })
-  }
+    });
+  };
 
-  handleOpenEvent = (eventToOpen) => () => {
+  handleOpenEvent = eventToOpen => () => {
     this.setState({
       selectedEvent: eventToOpen,
       isOpen: true
     });
   };
 
-  handleCreateEvent = (newEvent) => {
+  handleCreateEvent = newEvent => {
     newEvent.id = cuid();
     newEvent.hostPhotoURL = '/assets/user.png';
-    const updatedEvents = [...this.state.events, newEvent];
+    this.props.createEvent(newEvent);
     this.setState({
-      events: updatedEvents,
+      // events: updatedEvents,
       isOpen: false
     });
   };
 
   handleDeleteEvent = (eventId) => () => {
-    const updatedEvents = this.state.events.filter(e => e.id !== eventId);
-    this.setState({
-      events: updatedEvents
-    });
+this.props.deleteEvent(eventId);
   };
 
   render() {
@@ -89,4 +87,4 @@ class EventDashboard extends Component {
   }
 }
 
-export default connect(mapState)(EventDashboard);
+export default connect(mapState, actions)(EventDashboard);
