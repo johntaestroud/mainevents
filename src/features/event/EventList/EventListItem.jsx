b63a95a7eb36f8f712ff3243e106ddc028069ba2
Johntae Stroud <johntae.stroud@gmail.com>
2018-06-10 20:31:47 -0400

    Implemented detailed page structure

--- src/features/event/EventList/EventListItem.jsx
+++ src/features/event/EventList/EventListItem.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import { Segment, Item, Icon, List, Button } from 'semantic-ui-react';
+import { Link } from 'react-router-dom';
 import EventListAttendee from './EventListAttendee';
 
  class EventListItem extends Component {
@@ -37,7 +38,7 @@ import EventListAttendee from './EventListAttendee';
               <Segment clearing>
               <span>{event.description}</span>
                 <Button onClick={deleteEvent(event.id)} as="a" color="red" floated="right" content="Delete" />
-                <Button onClick={onEventOpen(event)} as="a" color="blue" floated="right" content="View" />
+                <Button as={Link} to={`/event/${event.id}`} color="blue" floated="right" content="View" />
               </Segment>
             </Segment.Group>
     )
