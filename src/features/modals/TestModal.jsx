ac7fab658af9435616804edcb6e98557d30f5fea
unknown <johntae.stroud@gmail.com>
2018-07-01 08:11:41 -0400

    Finishing modals setup in Redux

--- src/features/modals/TestModal.jsx
+++ src/features/modals/TestModal.jsx
@@ -1,9 +1,16 @@
 import React from 'react';
+import { connect } from 'react-redux';
 import { Modal } from 'semantic-ui-react';
+import { closeModal } from './modalActions';
 
-const TestModal = () => {
+const actions = {
+  closeModal
+}
+
+
+const TestModal = ({closeModal}) => {
   return (
-        <Modal closeIcon="close" open={true}>
+        <Modal closeIcon="close" open={true} onClose={closeModal}>
           <Modal.Header>Test Modal</Modal.Header>
           <Modal.Content>
             <Modal.Description>
@@ -14,5 +21,5 @@ const TestModal = () => {
   )
 }
 
-export default TestModal
+export default connect(null, actions)(TestModal)
 
--- src/features/testarea/TestComponent.jsx
+++ src/features/testarea/TestComponent.jsx
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import GoogleMapReact from 'google-map-react';
-import { Button, Icon } from 'semantic-ui-react';
+//import GoogleMapReact from 'google-map-react';
+import { Button } from 'semantic-ui-react';
 import Script from 'react-load-script';
 import PlacesAutocomplete, { geocodeByAddress, getLatLng } from 'react-places-autocomplete'
 import { incrementCounter, decrementCounter } from './testActions';
+import { openModal } from '../modals/modalActions';
 
 const mapState = (state) => ({
   data: state.test.data
@@ -12,10 +13,11 @@ const mapState = (state) => ({
 
 const actions = {
   incrementCounter, 
-  decrementCounter
-}
+  decrementCounter,
+  openModal
+};
 
-const Marker = () => <Icon name='marker' size='big' color='red'/>
+//const Marker = () => <Icon name='marker' size='big' color='red'/>
 
 class TestComponent extends Component {
   static defaultProps = {
@@ -50,7 +52,7 @@ class TestComponent extends Component {
       value: this.state.address,
       onChange: this.onChange,
     }
-    const {incrementCounter, decrementCounter, data} = this.props;
+    const {incrementCounter, decrementCounter, data, openModal } = this.props;
     return (
       <div>
         <Script //remove if error states multiple uses
@@ -61,13 +63,15 @@ class TestComponent extends Component {
         <h3>The answer is: {data}</h3>
         <Button onClick={incrementCounter} color='green' content='Increment'/>
         <Button onClick={decrementCounter} color='red' content='Decrement'/>
+        <Button onClick={() => openModal('TestModal', {data: 43})} color='blue' content='Open Modal'/>
+
         <br/><br/>
         <form onSubmit={this.handleFormSubmit}>
         {this.state.scriptLoaded && <PlacesAutocomplete inputProps={inputProps} />}
         <button type="submit">Submit</button>
       </form>
 
-         <div style={{ height: '300px', width: '100%' }}>
+         {/* <div style={{ height: '300px', width: '100%' }}>
           <GoogleMapReact
             bootstrapURLKeys={{ key: 'AIzaSyDqaN5EsfPICUmUUiNRT02xYfQSiVvKpAU' }}
             defaultCenter={this.props.center}
@@ -79,7 +83,7 @@ class TestComponent extends Component {
               text={'Kreyser Avrora'}
             />
           </GoogleMapReact>
-      </div>
+      </div> */}
 
       </div>
     );
