e977f5600ff6930d88f61ab17460cd6f6ee4e913
unknown <johntae.stroud@gmail.com>
2018-08-04 19:34:18 -0400

    Managed user profile data

--- src/features/auth/SocialLogin/SocilaLogin.jsx
+++ src/features/auth/SocialLogin/SocilaLogin.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
-import {Button, Icon  } from 'semantic-ui-react';
+import {Button, Icon } from 'semantic-ui-react';
 
-const SocilaLogin = () => {
+const SocilaLogin = ({socialLogin}) => {
   return (
         <div>
-          <Button type="button" style={{ marginBottom: '10px' }} fluid color="facebook">
+          <Button onClick={() => socialLogin('facebook')} type="button" style={{ marginBottom: '10px' }} fluid color="facebook">
             <Icon name="facebook" /> Login with Facebook
           </Button>
     
-          <Button type="button" fluid color="google plus">
+          <Button onClick={() => socialLogin('google')} type="button" fluid color="google plus">
             <Icon name="google plus" />
             Login with Google
           </Button>
